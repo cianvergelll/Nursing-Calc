d29b7def2cb78345c820e8009bb25d4dd0c68630
Ian <cianvergelll@gmail.com>
2025-06-06 11:36:27 +0800

    dilution calculator

--- src/routes/calculators/dilution/+page.svelte
+++ src/routes/calculators/dilution/+page.svelte
@@ -1,14 +1,19 @@
 <script>
-    import { FontAwesomeIcon } from "@fortawesome/svelte-fontawesome";
-    import Navbar from "../../component/Navbar.svelte";
+	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
+	import Navbar from '../../component/Navbar.svelte';
+
+	let submitted = $state(false);
 </script>
 
 <Navbar />
 
-<div class="w-[50%] flex flex-col mx-auto">
-  <p class="font-bold text-3xl mt-8 ml-5">Dilution Calculator</p>
-    <div class="w-full my-5 border border-gray-300 py-4 rounded-xl shadow-lg">
-        <p class="text-lg mx-5 font-medium">Important</p>
-        <p class="mx-5">This calculator is for educational purposes. Always verify calculations and follow your institution's protocols.</p>
-    </div>
+<div class="mx-auto flex w-[50%] flex-col">
+	<p class="mt-8 ml-5 text-3xl font-bold">Dilution Calculator</p>
+	<div class="my-5 w-full rounded-xl border border-gray-300 py-4 shadow-lg">
+		<p class="mx-5 text-lg font-medium">Important</p>
+		<p class="mx-5">
+			This calculator is for educational purposes. Always verify calculations and follow your
+			institution's protocols.
+		</p>
+	</div>
 </div>
